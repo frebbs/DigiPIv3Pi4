0db288282e6c25a97ba43c552420166632853747
Aaron Diable <aaron.diable@gmail.com>
2020-05-16 16:08:11 +0100

    Initial commit. This might become DigiPiV3 on the RPI4

---
+++ demon.js
@@ -0,0 +1,51 @@
+const workingDirRoot = '/Users/aaron/DevFolder/DigiPIv3Pi4/workingPiDir/';
+const workingDirUploads = '/Users/aaron/DevFolder/DigiPIv3Pi4/workingPiDir/uploads';
+const workingDirCurrentlyPlaying = '/Users/aaron/DevFolder/DigiPIv3Pi4/workingPiDir/playing';
+
+const fs = require('fs');
+
+// bring in Chokidar
+const chokidar = require('chokidar');
+
+const uploadWatcher = chokidar.watch(workingDirUploads, {
+    ignored: /(^|[\/\\])\../, // ignore dotfiles
+    persistent: true
+  });
+
+const playingNowWatcher = chokidar.watch(workingDirCurrentlyPlaying, {
+    ignored: /(^|[\/\\])\../, // ignore dotfiles
+    persistent: true
+})
+
+//   uploadWatcher.on('created', (path) => {
+//     console.log(`A file was changed: ${path}`);
+//   })
+
+// Checks the directory for changes and takes the approprite action
+
+uploadWatcher.on('raw', (event, path, details) => {
+    if (event == 'created' || event == 'modifed' ) {
+
+        let filenamePath = path.split('/');
+        let fileName = filenamePath[filenamePath.length - 1]
+        let newFilePath = workingDirCurrentlyPlaying + '/' + fileName;
+
+        // Move the uploaded file to the playing now directory (This deletes it from the updloads)
+        fs.rename(path, newFilePath, (err) => {
+            if (err) console.log(err)
+            console.log('File deleted and moved')
+        })
+    }
+})
+
+// Checks the directory for changes and takes the approprite action
+
+playingNowWatcher.on('raw', (event, path, detais) => {
+    let filenamePath = path.split('/');
+    let fileName = filenamePath[filenamePath.length - 1]
+
+    // This is where I will tell the pi what to do with the new file
+    if (event == 'moved') {
+        console.log(`A file as been moved or updated: ${fileName}`)
+    }
+})
